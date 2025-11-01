e9552b890024ec108f8650db5d8a6dc74dd9495b
Billy Li <li000400@umn.edu>
2025-11-01 20:02:07 +0000

    change style to show which lines have the errors

--- data_processing/cld_root2hdf5.py
+++ data_processing/cld_root2hdf5.py
@@ -58,142 +58,148 @@ def process_root_files(input_dir, output_dir, events_per_hdf5=None, max_events=N
         desc="Processing ROOT files",
         total=max_root_files or len(list(Path(input_dir).rglob("*.root"))),
     ):
-        try:
-            fi = uproot.open(root_file)
-            collectionIDs = {
-                k: v
-                for k, v in zip(
-                    fi.get("podio_metadata").arrays("events___idTable/m_names")["events___idTable/m_names"][0],
-                    fi.get("podio_metadata").arrays("events___idTable/m_collectionIDs")[
-                        "events___idTable/m_collectionIDs"
-                    ][0],
-                )
-            }
-            ev = fi["events"]
-            event_data = get_event_data(ev)
+        # try:
+        fi = uproot.open(root_file)
+        collectionIDs = {
+            k: v
+            for k, v in zip(
+                fi.get("podio_metadata").arrays("events___idTable/m_names")["events___idTable/m_names"][0],
+                fi.get("podio_metadata").arrays("events___idTable/m_collectionIDs")[
+                    "events___idTable/m_collectionIDs"
+                ][0],
+            )
+        }
+        ev = fi["events"]
+        event_data = get_event_data(ev)
 
-            if one_file_per_root:
-                output_file = output_dir / Path(f"{root_file.stem}.hdf5")
-                h5f = h5py.File(output_file, "w")
-                print(f"Created new HDF5 file: {output_file}")
-            else:
-                output_file = output_dir / Path(f"events_{event_counter}_to_{event_counter + events_per_hdf5 - 1}.hdf5")
-                h5f = h5py.File(output_file, "w")
-                print(f"Created new HDF5 file: {output_file}")
+        if one_file_per_root:
+            output_file = output_dir / Path(f"{root_file.stem}.hdf5")
+            h5f = h5py.File(output_file, "w")
+            print(f"Created new HDF5 file: {output_file}")
+        else:
+            output_file = output_dir / Path(f"events_{event_counter}_to_{event_counter + events_per_hdf5 - 1}.hdf5")
+            h5f = h5py.File(output_file, "w")
+            print(f"Created new HDF5 file: {output_file}")
 
-            for iev in range(len(ev["MCParticles.momentum.x"].array())):
-                # Create a group for the event
-                event_group = h5f.create_group(f"event_{event_counter}")
+        for iev in range(len(ev["MCParticles.momentum.x"].array())):
+            # Create a group for the event
+            event_group = h5f.create_group(f"event_{event_counter}")
 
-                # Extract features and adjacency matrices for the current event
-                gen_features = gen_to_features(event_data, iev)
-                track_features = track_to_features(event_data, iev)
-                cluster_features = cluster_to_features(
-                    event_data, iev, cluster_features=["position.x", "position.y", "position.z", "energy", "type"]
-                )
-                calo_hit_features, genparticle_to_calo_hit_matrix, _ = process_calo_hit_data(
-                    event_data, iev, collectionIDs
-                )
-                tracker_hit_features, genparticle_to_tracker_hit_matrix, _ = process_tracker_hit_data(
-                    event_data, iev, collectionIDs
-                )
-                track_to_tracker_hit_matrix, _ = create_track_to_hit_coo_matrix(event_data, iev, collectionIDs)
-                cluster_to_cluster_hit_matrix, _ = create_cluster_to_hit_coo_matrix(event_data, iev, collectionIDs)
-                gp_to_track_matrix = genparticle_track_adj(event_data, iev)
-                gp_to_gp = create_genparticle_to_genparticle_coo_matrix(event_data, iev)
+            # Extract features and adjacency matrices for the current event
+            gen_features = gen_to_features(event_data, iev)
+            track_features = track_to_features(event_data, iev)
+            cluster_features = cluster_to_features(
+                event_data, iev, cluster_features=["position.x", "position.y", "position.z", "energy", "type"]
+            )
+            calo_hit_features, genparticle_to_calo_hit_matrix, _ = process_calo_hit_data(
+                event_data, iev, collectionIDs
+            )
+            # print("calo_hit_features", calo_hit_features)
+            # print("genparticle_to_calo_hit_matrix", genparticle_to_calo_hit_matrix)
+            tracker_hit_features, genparticle_to_tracker_hit_matrix, _ = process_tracker_hit_data(
+                event_data, iev, collectionIDs
+            )
+            track_to_tracker_hit_matrix, _ = create_track_to_hit_coo_matrix(event_data, iev, collectionIDs)
+            # print(track_to_tracker_hit_matrix)
+            cluster_to_cluster_hit_matrix, _ = create_cluster_to_hit_coo_matrix(event_data, iev, collectionIDs)
+            # print("cluster_to_cluster_hit_matrix", cluster_to_cluster_hit_matrix)
+            gp_to_track_matrix = genparticle_track_adj(event_data, iev)
+            # print("gp_to_track_matrix", gp_to_track_matrix)
+            gp_to_gp = create_genparticle_to_genparticle_coo_matrix(event_data, iev)
+            # print("gp_to_gp", gp_to_gp)
 
-                # Save genparticle features
-                for key, value in gen_features.items():
-                    event_group.create_dataset(f"gen_features/{key}", data=value)
+            # Save genparticle features
+            for key, value in gen_features.items():
+                event_group.create_dataset(f"gen_features/{key}", data=value)
 
-                # Save track features
-                for key, value in track_features.items():
-                    event_group.create_dataset(f"track_features/{key}", data=value)
+            # Save track features
+            for key, value in track_features.items():
+                event_group.create_dataset(f"track_features/{key}", data=value)
 
-                # Save cluster features
-                for key, value in cluster_features.items():
-                    event_group.create_dataset(f"cluster_features/{key}", data=value)
+            # Save cluster features
+            for key, value in cluster_features.items():
+                event_group.create_dataset(f"cluster_features/{key}", data=value)
 
-                # Save calorimeter hit features
-                for key, value in calo_hit_features.items():
-                    event_group.create_dataset(f"calo_hit_features/{key}", data=awkward.to_numpy(value))
+            # Save calorimeter hit features
+            for key, value in calo_hit_features.items():
+                event_group.create_dataset(f"calo_hit_features/{key}", data=awkward.to_numpy(value))
 
-                # Save tracker hit features
-                for key, value in tracker_hit_features.items():
-                    event_group.create_dataset(f"tracker_hit_features/{key}", data=awkward.to_numpy(value))
+            # Save tracker hit features
+            for key, value in tracker_hit_features.items():
+                event_group.create_dataset(f"tracker_hit_features/{key}", data=awkward.to_numpy(value))
 
-                # Save adjacency matrices
-                event_group.create_dataset(
-                    "genparticle_to_calo_hit_matrix/rows", data=genparticle_to_calo_hit_matrix[0]
-                )
-                event_group.create_dataset(
-                    "genparticle_to_calo_hit_matrix/cols", data=genparticle_to_calo_hit_matrix[1]
-                )
-                event_group.create_dataset(
-                    "genparticle_to_calo_hit_matrix/weights", data=genparticle_to_calo_hit_matrix[2]
-                )
+            # Save adjacency matrices
+            event_group.create_dataset(
+                "genparticle_to_calo_hit_matrix/rows", data=genparticle_to_calo_hit_matrix["gen_idx"]
+            )
+            event_group.create_dataset(
+                "genparticle_to_calo_hit_matrix/cols", data=genparticle_to_calo_hit_matrix["hit_idx"]
+            )
+            event_group.create_dataset(
+                "genparticle_to_calo_hit_matrix/weights", data=genparticle_to_calo_hit_matrix["weight"]
+            )
 
-                event_group.create_dataset(
-                    "genparticle_to_tracker_hit_matrix/rows", data=genparticle_to_tracker_hit_matrix[0]
-                )
-                event_group.create_dataset(
-                    "genparticle_to_tracker_hit_matrix/cols", data=genparticle_to_tracker_hit_matrix[1]
-                )
-                event_group.create_dataset(
-                    "genparticle_to_tracker_hit_matrix/weights", data=genparticle_to_tracker_hit_matrix[2]
-                )
+            event_group.create_dataset(
+                "genparticle_to_tracker_hit_matrix/rows", data=genparticle_to_tracker_hit_matrix["gen_idx"]
+            )
+            event_group.create_dataset(
+                "genparticle_to_tracker_hit_matrix/cols", data=genparticle_to_tracker_hit_matrix["hit_idx"]
+            )
+            event_group.create_dataset(
+                "genparticle_to_tracker_hit_matrix/weights", data=genparticle_to_tracker_hit_matrix["weight"]
+            )
 
-                event_group.create_dataset("track_to_tracker_hit_matrix/rows", data=track_to_tracker_hit_matrix[0])
-                event_group.create_dataset("track_to_tracker_hit_matrix/cols", data=track_to_tracker_hit_matrix[1])
-                event_group.create_dataset("track_to_tracker_hit_matrix/weights", data=track_to_tracker_hit_matrix[2])
+            event_group.create_dataset("track_to_tracker_hit_matrix/rows", data=track_to_tracker_hit_matrix["track_idx"])
+            event_group.create_dataset("track_to_tracker_hit_matrix/cols", data=track_to_tracker_hit_matrix["hit_idx"])
+            event_group.create_dataset("track_to_tracker_hit_matrix/weights", data=track_to_tracker_hit_matrix["weight"])
 
-                event_group.create_dataset("cluster_to_cluster_hit_matrix/rows", data=cluster_to_cluster_hit_matrix[0])
-                event_group.create_dataset("cluster_to_cluster_hit_matrix/cols", data=cluster_to_cluster_hit_matrix[1])
-                event_group.create_dataset(
-                    "cluster_to_cluster_hit_matrix/weights", data=cluster_to_cluster_hit_matrix[2]
-                )
+            event_group.create_dataset("cluster_to_cluster_hit_matrix/rows", data=cluster_to_cluster_hit_matrix["cluster_idx"])
+            event_group.create_dataset("cluster_to_cluster_hit_matrix/cols", data=cluster_to_cluster_hit_matrix["hit_idx"])
+            event_group.create_dataset(
+                "cluster_to_cluster_hit_matrix/weights", data=cluster_to_cluster_hit_matrix["weight"]
+            )
 
-                event_group.create_dataset("gp_to_track_matrix/rows", data=gp_to_track_matrix[0])
-                event_group.create_dataset("gp_to_track_matrix/cols", data=gp_to_track_matrix[1])
-                event_group.create_dataset("gp_to_track_matrix/weights", data=gp_to_track_matrix[2])
+            event_group.create_dataset("gp_to_track_matrix/rows", data=gp_to_track_matrix[0])
+            event_group.create_dataset("gp_to_track_matrix/cols", data=gp_to_track_matrix[1])
+            event_group.create_dataset("gp_to_track_matrix/weights", data=gp_to_track_matrix[2])
 
-                event_group.create_dataset("gp_to_gp/rows", data=gp_to_gp[0])
-                event_group.create_dataset("gp_to_gp/cols", data=gp_to_gp[1])
-                event_group.create_dataset("gp_to_gp/weights", data=gp_to_gp[2])
+            event_group.create_dataset("gp_to_gp/rows", data=gp_to_gp["parent_idx"])
+            event_group.create_dataset("gp_to_gp/cols", data=gp_to_gp["daughter_idx"])
+            event_group.create_dataset("gp_to_gp/weights", data=gp_to_gp["weight"])
 
-                event_counter += 1
+            event_counter += 1
 
-                if max_events is not None and event_counter >= max_events:
-                    print(f"Reached max_events limit: {max_events}. Stopping processing.")
-                    if h5f:
-                        h5f.close()
-                    return
-
-                if not one_file_per_root and event_counter % events_per_hdf5 == 0:
-                    if h5f:
-                        h5f.close()
-                    output_file = output_dir / Path(
-                        f"events_{event_counter}_to_{event_counter + events_per_hdf5 - 1}.hdf5"
-                    )
-                    h5f = h5py.File(output_file, "w")
-                    print(f"Created new HDF5 file: {output_file}")
-
-            print(f"Processed: {root_file}")
-            root_counter += 1
-
-            if max_root_files is not None and root_counter >= max_root_files:
-                print(f"Reached max_root_files limit: {max_root_files}. Stopping processing.")
+            if max_events is not None and event_counter >= max_events:
+                print(f"Reached max_events limit: {max_events}. Stopping processing.")
                 if h5f:
                     h5f.close()
                 return
 
-            # Close the HDF5 file for the current ROOT file if the flag is set
-            if one_file_per_root and h5f:
-                h5f.close()
+            if not one_file_per_root and event_counter % events_per_hdf5 == 0:
+                if h5f:
+                    h5f.close()
+                output_file = output_dir / Path(
+                    f"events_{event_counter}_to_{event_counter + events_per_hdf5 - 1}.hdf5"
+                )
+                h5f = h5py.File(output_file, "w")
+                print(f"Created new HDF5 file: {output_file}")
 
-        except Exception as e:
-            print(f"Error processing {root_file}: {e}")
-            break
+        print(f"Processed: {root_file}")
+        root_counter += 1
+
+        if max_root_files is not None and root_counter >= max_root_files:
+            print(f"Reached max_root_files limit: {max_root_files}. Stopping processing.")
+            if h5f:
+                h5f.close()
+            return
+
+        # Close the HDF5 file for the current ROOT file if the flag is set
+        if one_file_per_root and h5f:
+            h5f.close()
+
+        # except Exception as e:
+        #     print(f"Error processing {root_file}: {e}")
+        #     break
 
     # Close the last HDF5 file if one_file_per_root is False
     if h5f and not one_file_per_root:
@@ -213,6 +219,7 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
+    print(args)
 
     # Ensure output directory exists
     output_dir = Path(args.output_dir)
